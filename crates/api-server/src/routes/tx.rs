f3fdfc8b8c8b85914bd2ca8a4d1d43abad345091
Ubuntu <ecopello@gmail.com>
2025-01-22 02:05:26 +0000

    Fix from registry

--- crates/api-server/src/routes/tx.rs
+++ crates/api-server/src/routes/tx.rs
@@ -169,7 +169,7 @@ mod tests {
             Arc::new(Vec::new()).to_vec(),
         );
         SystemRegistry::set(mempool_service.start());
-        let mempool_addr = MempoolService::from_registry();
+        let mempool_addr = <MempoolService as actix::SystemService>::from_registry();
 
         let chunk_provider = ChunkProvider::new(
             storage_config.clone(),
