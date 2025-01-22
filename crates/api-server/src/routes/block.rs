d3fb6f9c68ab8056bed6d6618f4950a4428ada5a
Ubuntu <ecopello@gmail.com>
2025-01-22 02:08:38 +0000

    Cargo fix

--- crates/api-server/src/routes/block.rs
+++ crates/api-server/src/routes/block.rs
@@ -33,7 +33,7 @@ mod tests {
     use crate::routes;
 
     use super::*;
-    use actix::{Actor, ArbiterService, Registry, SystemRegistry};
+    use actix::{Actor, SystemRegistry};
     use actix_web::{middleware::Logger, test, App, Error};
     use awc::http::StatusCode;
     use base58::ToBase58;
--- crates/api-server/src/routes/tx.rs
+++ crates/api-server/src/routes/tx.rs
@@ -125,7 +125,7 @@ mod tests {
     use crate::routes;
 
     use super::*;
-    use actix::{Actor, ArbiterService, Registry, SystemRegistry};
+    use actix::{Actor, SystemRegistry};
     use actix_web::{middleware::Logger, test, App, Error};
     use base58::ToBase58;
     use database::open_or_create_db;
