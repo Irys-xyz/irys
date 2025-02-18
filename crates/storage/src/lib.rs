d6f3c94022eb2f3b47c87b4c996adfcc1cbf07c7
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2025-02-18 16:28:47 -0300

    reformat storage/lib.rs

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -5,5 +5,5 @@ pub use nodit::Interval;
 pub mod chunk_provider;
 pub mod storage_module;
 pub use storage_module::*;
-pub mod reth_provider;
 pub mod irys_consensus_data_db;
+pub mod reth_provider;
