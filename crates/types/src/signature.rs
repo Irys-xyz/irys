dde164a09004231dec8c8526faba804f7e2ce946
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-03-03 14:45:17 -0500

    [WIP] unnecessary map_or

--- crates/types/src/signature.rs
+++ crates/types/src/signature.rs
@@ -40,9 +40,7 @@ impl IrysSignature {
     /// Validates this signature by performing signer recovery  
     /// NOTE: This will silently short circuit to `false` if any part of the recovery operation errors
     pub fn validate_signature(&self, prehash: [u8; 32], expected_address: Address) -> bool {
-        recover_signer(&self.0, prehash.into()).map_or(false, |recovered_address| {
-            expected_address == recovered_address
-        })
+        recover_signer(&self.0, prehash.into()) == Some(expected_address)
     }
 }
 
