18d420a595a6afb30b9d155fe3025e87f60b3aee
Roberts Pumpurs <roberts@pumpurlabs.com>
2025-03-13 11:22:26 +0200

    fix: U256 Compact encoding

--- crates/types/src/block.rs
+++ crates/types/src/block.rs
@@ -374,6 +374,8 @@ impl IrysBlockHeader {
             miner_address: Address::ZERO,
             oracle_irys_price: Amount::token(dec!(1.0))
                 .expect("dec!(1.0) must evaluate to a valid token amount"),
+            ema_irys_price: Amount::token(dec!(1.0))
+                .expect("dec!(1.0) must evaluate to a valid token amount"),
             ..Default::default()
         }
     }
@@ -414,7 +416,7 @@ mod tests {
     }
 
     #[test]
-    fn test_vdf_limiter_info_compact_round_trip() {
+    fn test_vdf_limiter_info_rlp_round_trip() {
         let data = VDFLimiterInfo {
             output: H256::random(),
             global_step_number: 42,
@@ -431,6 +433,29 @@ mod tests {
         let mut buffer = vec![];
         data.encode(&mut buffer);
         let decoded = Decodable::decode(&mut buffer.as_slice()).unwrap();
+
+        // Assert
+        assert_eq!(data, decoded);
+    }
+
+    #[test]
+    fn test_vdf_limiter_info_compact_round_trip() {
+        let data = VDFLimiterInfo {
+            output: H256::random(),
+            global_step_number: 42,
+            seed: H256::random(),
+            next_seed: H256::random(),
+            prev_output: H256::random(),
+            last_step_checkpoints: H256List(vec![H256::random(), H256::random()]),
+            steps: H256List(vec![H256::random(), H256::random()]),
+            vdf_difficulty: Some(123),
+            next_vdf_difficulty: Some(321),
+        };
+
+        // action
+        let mut buffer = vec![];
+        data.to_compact(&mut buffer);
+        let (decoded, ..) = VDFLimiterInfo::from_compact(&mut buffer.as_slice(), buffer.len());
 
         // Assert
         assert_eq!(data, decoded);
