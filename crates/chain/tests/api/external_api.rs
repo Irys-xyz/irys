503f500dc7add621e99b4ed09b4e933f3ef81042
Craig Mayhew <craig@mayhew.io>
2025-03-20 15:46:27 +0000

    Remove unused code

--- crates/chain/tests/api/external_api.rs
+++ crates/chain/tests/api/external_api.rs
@@ -12,25 +12,9 @@ use tracing::info;
 
 #[actix::test]
 async fn external_api() -> eyre::Result<()> {
-    let temp_dir = setup_tracing_and_temp_dir(Some("external_api"), false);
-    info!("tracing enabled");
-    let mut node_config = IrysNodeConfig::default();
-    node_config.base_directory = temp_dir.path().to_path_buf();
-
-    let testnet_config = Config::testnet();
-    let mut storage_config = StorageConfig::new(&testnet_config);
-    storage_config.num_chunks_in_partition = 6;
-
-    let _chunk_size = storage_config.chunk_size;
-
-    let ctx = setup().await?;
+    let _ctx = setup().await?;
 
     let address = "http://127.0.0.1:8080";
-    // TODO: remove this delay and use proper probing to check if the server is active
-    sleep(Duration::from_millis(500)).await;
-
-    // server should be running
-    // check with request to `/v1/info`
     let client = awc::Client::default();
 
     let response = client
