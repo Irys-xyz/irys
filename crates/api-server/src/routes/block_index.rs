712aab153d80a5eef15053e511ac6addc2650f5a
Craig Mayhew <craig@mayhew.io>
2025-03-21 16:19:58 +0000

    end point now returns 50 BlockIndexItems

---
+++ crates/api-server/src/routes/block_index.rs
@@ -0,0 +1,23 @@
+use crate::ApiState;
+use actix_web::{http::header::ContentType, web, HttpResponse};
+use irys_database::BlockIndexItem;
+
+pub async fn block_index_route(state: web::Data<ApiState>) -> HttpResponse {
+    let block_index = state.block_index.clone().expect("block index");
+
+    let limit = 50;
+    let start = 5;
+
+    let read = block_index.read();
+    let requested_blocks: Vec<&BlockIndexItem> = read
+        .items
+        .into_iter()
+        .enumerate()
+        .filter(|(i, _)| *i >= start && *i < start + limit)
+        .map(|(_, block)| block)
+        .collect();
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(serde_json::to_string_pretty(&requested_blocks).unwrap())
+}
