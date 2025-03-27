67cff95c646421e98d9f357a33d22d62ac7c32df
Ernius <ecopello@gmail.com>
2025-03-27 10:57:20 -0300

    Cargo fix

--- crates/chain/tests/promotion/data_promotion_basic.rs
+++ crates/chain/tests/promotion/data_promotion_basic.rs
@@ -17,9 +17,9 @@ async fn heavy_data_promotion_test() {
     use irys_actors::packing::wait_for_packing;
     use irys_api_server::{routes, ApiState};
     use irys_database::DataLedger;
-    use irys_testing_utils::utils::setup_tracing_and_temp_dir;
+    
     use irys_types::{
-        irys::IrysSigner, IrysTransaction, IrysTransactionHeader, LedgerChunkOffset, StorageConfig,
+        irys::IrysSigner, IrysTransaction, IrysTransactionHeader, LedgerChunkOffset,
     };
     use reth_primitives::GenesisAccount;
     use std::time::Duration;
