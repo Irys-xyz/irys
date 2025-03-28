8b29140a1746d2a5b24602a67fe8250004bafb9a
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-03-28 12:58:10 +0000

    chore: disable verbose block_index_service logging

--- crates/actors/src/block_index_service.rs
+++ crates/actors/src/block_index_service.rs
@@ -192,23 +192,23 @@ impl BlockIndexService {
 
         self.num_blocks += 1;
 
-        if self.num_blocks % 10 == 0 {
-            let mut prev_entry: Option<&BlockLogEntry> = None;
-            info!("block_height, block_time(ms), difficulty");
-            for entry in &self.block_log {
-                let duration = if let Some(pe) = prev_entry {
-                    if entry.timestamp >= pe.timestamp {
-                        Duration::from_millis((entry.timestamp - pe.timestamp) as u64)
-                    } else {
-                        Duration::from_millis(0)
-                    }
-                } else {
-                    Duration::from_millis(0)
-                };
-                info!("{}, {:?}, {}", entry.height, duration, entry.difficulty);
-                prev_entry = Some(entry);
-            }
-        }
+        // if self.num_blocks % 10 == 0 {
+        //     let mut prev_entry: Option<&BlockLogEntry> = None;
+        //     info!("block_height, block_time(ms), difficulty");
+        //     for entry in &self.block_log {
+        //         let duration = if let Some(pe) = prev_entry {
+        //             if entry.timestamp >= pe.timestamp {
+        //                 Duration::from_millis((entry.timestamp - pe.timestamp) as u64)
+        //             } else {
+        //                 Duration::from_millis(0)
+        //             }
+        //         } else {
+        //             Duration::from_millis(0)
+        //         };
+        //         info!("{}, {:?}, {}", entry.height, duration, entry.difficulty);
+        //         prev_entry = Some(entry);
+        //     }
+        // }
     }
 }
 
