dc180608372db216a7aafa3e01b0300950fef2e3
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2025-04-02 12:56:59 -0300

    fix post /version

--- crates/types/src/version.rs
+++ crates/types/src/version.rs
@@ -142,7 +142,7 @@ impl Default for VersionRequest {
     }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Arbitrary)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, Eq, PartialEq, Arbitrary)]
 pub struct PeerAddress {
     pub gossip: SocketAddr,
     pub api: SocketAddr,
