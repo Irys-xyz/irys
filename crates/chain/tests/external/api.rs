57052c5186fd2d812d719ce284d98826a773420e
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-04-08 10:07:04 +0000

    feate: Update external API test to use new testing code

--- crates/chain/tests/external/api.rs
+++ crates/chain/tests/external/api.rs
@@ -1,15 +1,11 @@
-use crate::utils::{capacity_chunk_solution, future_or_mine_on_timeout};
+use crate::utils::{future_or_mine_on_timeout, mine_blocks, IrysNodeTest};
 use actix_http::StatusCode;
 use alloy_core::primitives::U256;
 use base58::ToBase58;
 use irys_actors::mempool_service::GetBestMempoolTxs;
 use irys_actors::packing::wait_for_packing;
-use irys_actors::SolutionFoundMessage;
 use irys_api_server::routes::tx::TxOffset;
-use irys_chain::start_irys_node;
-use irys_config::IrysNodeConfig;
 use irys_database::tables::IngressProofs;
-use irys_testing_utils::utils::setup_tracing_and_temp_dir;
 use irys_types::{irys::IrysSigner, Address, Config};
 use reth_db::transaction::DbTx;
 use reth_db::Database as _;
@@ -31,19 +27,16 @@ const DEV_ADDRESS: &str = "64f1a2829e0e698c18e7792d6e74f67d89aa0a32";
 /// that's it!, just kill this test once the JS client test finishes.
 async fn external_api() -> eyre::Result<()> {
     std::env::set_var("RUST_LOG", "debug,irys_actors::mining=error,irys_actors::packing=error,irys_chain::vdf=off,irys_vdf::vdf_state=off");
-    let temp_dir = setup_tracing_and_temp_dir(Some("external_api"), false);
 
     let mut testnet_config = Config::testnet();
     testnet_config.port = 8080; // external test, should never be run concurrently
 
-    let mut config = IrysNodeConfig::new(&testnet_config);
-    config.base_directory = temp_dir.path().to_path_buf();
-
-    let storage_config = irys_types::StorageConfig::new(&testnet_config);
-    let main_address = config.mining_signer.address();
     let account1 = IrysSigner::random_signer(&testnet_config);
 
-    config.extend_genesis_accounts(vec![
+    let mut node = IrysNodeTest::new_genesis(testnet_config.clone());
+    let main_address = node.cfg.irys_node_config.mining_signer.address();
+
+    node.cfg.irys_node_config.extend_genesis_accounts(vec![
         (
             main_address,
             GenesisAccount {
@@ -66,16 +59,16 @@ async fn external_api() -> eyre::Result<()> {
             },
         ),
     ]);
+    let node = node.start().await;
 
-    let node = start_irys_node(config, storage_config, testnet_config.clone()).await?;
-    node.actor_addresses.stop_mining()?;
+    node.node_ctx.actor_addresses.stop_mining()?;
     wait_for_packing(
-        node.actor_addresses.packing.clone(),
+        node.node_ctx.actor_addresses.packing.clone(),
         Some(Duration::from_secs(10)),
     )
     .await?;
 
-    let http_url = format!("http://127.0.0.1:{}", node.config.port);
+    let http_url = format!("http://127.0.0.1:{}", node.cfg.config.port);
 
     // server should be running
     // check with request to `/v1/info`
@@ -88,12 +81,17 @@ async fn external_api() -> eyre::Result<()> {
         .unwrap();
 
     assert_eq!(response.status(), 200);
-    info!("HTTP server started on port {}", node.config.port);
+    info!("HTTP server started on port {}", node.cfg.config.port);
 
     info!("waiting for tx header...");
 
     let recv_tx = loop {
-        let txs = node.actor_addresses.mempool.send(GetBestMempoolTxs).await;
+        let txs = node
+            .node_ctx
+            .actor_addresses
+            .mempool
+            .send(GetBestMempoolTxs)
+            .await;
         match txs {
             Ok(transactions) if !transactions.is_empty() => {
                 break transactions[0].clone();
@@ -112,7 +110,7 @@ async fn external_api() -> eyre::Result<()> {
     // now we wait for an ingress proof to be generated for this tx (automatic once all chunks have been uploaded)
     let ingress_proof = loop {
         // don't reuse the tx! it has read isolation (won't see anything committed after it's creation)
-        let ro_tx = &node.db.0.tx().unwrap();
+        let ro_tx = &node.node_ctx.db.0.tx().unwrap();
         match ro_tx.get::<IngressProofs>(recv_tx.data_root).unwrap() {
             Some(ip) => break ip,
             None => sleep(Duration::from_millis(100)).await,
@@ -153,32 +151,17 @@ async fn external_api() -> eyre::Result<()> {
     });
 
     let _start_offset = future_or_mine_on_timeout(
-        node.clone(),
+        node.node_ctx.clone(),
         &mut start_offset_fut,
         Duration::from_millis(500),
-        node.vdf_steps_guard.clone(),
-        &node.vdf_config,
-        &node.storage_config,
+        node.node_ctx.vdf_steps_guard.clone(),
+        &node.node_ctx.vdf_config,
+        &node.node_ctx.storage_config,
     )
     .await?
     .unwrap();
 
-    for _i in 1..10 {
-        let poa_solution = capacity_chunk_solution(
-            node.node_config.mining_signer.address(),
-            node.vdf_steps_guard.clone(),
-            &node.vdf_config,
-            &node.storage_config,
-        )
-        .await;
-
-        let _ = node
-            .actor_addresses
-            .block_producer
-            .send(SolutionFoundMessage(poa_solution.clone()))
-            .await?
-            .unwrap();
-    }
+    mine_blocks(&node.node_ctx, 10).await?;
 
     // sleep so the client has a chance to read the chunks
     sleep(Duration::from_millis(100_000)).await;
