b76d233cadf9f55c0e23df4e1386a9e70bf1f6da
Craig Mayhew <craig@mayhew.io>
2025-04-10 23:58:31 +0100

    WIP rewriting to be a multi node test using existing genesis node

--- crates/chain/tests/startup/startup.rs
+++ crates/chain/tests/startup/startup.rs
@@ -1,11 +1,30 @@
-use crate::utils::{mine_block, IrysNodeTest};
+use crate::utils::{mine_block, random_port, IrysNodeTest};
 use irys_actors::block_tree_service::get_canonical_chain;
+use irys_types::Config;
 use std::time::Duration;
 
-#[test_log::test(tokio::test)]
+#[test_log::test(actix_web::test)]
 async fn heavy_test_can_resume_from_genesis_startup() -> eyre::Result<()> {
     // setup
-    let node = IrysNodeTest::default();
+    //
+    // genesis node
+    let mut testnet_genesis_config = Config::testnet();
+    let port = random_port().await?;
+    let trusted_peer = vec![format!("127.0.0.1:{}", port)
+        .parse()
+        .expect("valid SocketAddr expected")];
+    testnet_genesis_config.port = port;
+    testnet_genesis_config.trusted_peers = trusted_peer.clone();
+    let genesis_node = IrysNodeTest::new_genesis(testnet_genesis_config).await;
+    let genesis_ctx = genesis_node.start().await;
+    tokio::time::sleep(Duration::from_secs(2)).await;
+    mine_block(&genesis_ctx.node_ctx).await?;
+    //mine_block(&genesis_ctx.node_ctx).await?;
+    // peer node
+    let mut testnet_peer_config = Config::testnet();
+    testnet_peer_config.trusted_peers = trusted_peer;
+    //this next line errors
+    let node = IrysNodeTest::new(testnet_peer_config).await;
 
     // action:
     // 1. start the genesis node;
@@ -25,7 +44,7 @@ async fn heavy_test_can_resume_from_genesis_startup() -> eyre::Result<()> {
     assert_eq!(
         header_1.height,
         chain.last().unwrap().1,
-        "expect only the first manually miend block to be saved"
+        "expect only the first manually mined block to be saved"
     );
     assert_eq!(
         chain.len(),
@@ -38,6 +57,7 @@ async fn heavy_test_can_resume_from_genesis_startup() -> eyre::Result<()> {
         .unwrap();
     assert_eq!(chain.len(), 3, "we expect the genesis block + 2 new blocks");
 
+    genesis_ctx.stop().await;
     ctx.stop().await;
     Ok(())
 }
