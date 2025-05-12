7b50b6dad4fc8073484f0ff950db93a2a5a4d9ae
Craig Mayhew <craig@mayhew.io>
2025-05-12 14:56:12 +0100

    adjusting test to show reduced Seed messages

--- crates/actors/src/mining.rs
+++ crates/actors/src/mining.rs
@@ -647,14 +647,15 @@ mod tests {
             vdf_service.send(GetVdfStateMessage).await.unwrap();
 
         let hash: H256 = H256::random();
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)]));
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)]));
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)]));
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)]));
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)])); //5
-                                                        // reset
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)])); //6
-        vdf_service.do_send(VdfSeed(vec![Seed(hash)])); //7
+        vdf_service.do_send(VdfSeed(vec![
+            Seed(hash),
+            Seed(hash),
+            Seed(hash),
+            Seed(hash),
+            Seed(hash),
+        ])); //1 to 5
+             // reset
+        vdf_service.do_send(VdfSeed(vec![Seed(hash), Seed(hash)])); //6 and 7
 
         sleep(Duration::from_secs(1)).await;
 
