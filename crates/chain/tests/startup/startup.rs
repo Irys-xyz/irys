8804728eb15b2bde5fa0c2d9bd5227bdf526d9ef
Craig Mayhew <craig@mayhew.io>
2025-05-16 15:19:12 +0100

    reduce test flakyness with waits after restart and block mine

--- crates/chain/tests/startup/startup.rs
+++ crates/chain/tests/startup/startup.rs
@@ -24,6 +24,7 @@ async fn heavy_test_can_resume_from_genesis_startup_with_ctx() -> eyre::Result<(
     tokio::time::sleep(Duration::from_secs(2)).await;
     // restart the node
     let ctx = ctx.stop().await.start().await;
+    tokio::time::sleep(Duration::from_secs(2)).await;
 
     // assert -- expect that the non genesis node can continue with the genesis data
     let (chain, ..) = get_canonical_chain(ctx.node_ctx.block_tree_guard.clone())
@@ -40,6 +41,7 @@ async fn heavy_test_can_resume_from_genesis_startup_with_ctx() -> eyre::Result<(
         "we expect the genesis block + 1 new block (the second block does not get saved)"
     );
     mine_block(&ctx.node_ctx).await?;
+    tokio::time::sleep(Duration::from_secs(1)).await;
     let (chain, ..) = get_canonical_chain(ctx.node_ctx.block_tree_guard.clone())
         .await
         .unwrap();
