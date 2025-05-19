57a43144f4432dbf6a5a2bc5717a24c90ac67a6c
Craig Mayhew <craig@mayhew.io>
2025-05-19 14:28:30 +0100

    check for send error and panic if so

--- crates/chain/src/vdf.rs
+++ crates/chain/src/vdf.rs
@@ -204,9 +204,12 @@ mod tests {
             mocked_vdf_service(&config).await;
 
         let (oneshot_tx, oneshot_rx) = tokio::sync::oneshot::channel();
-        let _ = tx.send(VdfServiceMessage::GetVdfStateMessage {
+        if let Err(e) = tx.send(VdfServiceMessage::GetVdfStateMessage {
             response: oneshot_tx,
-        });
+        }) {
+            panic!("error: {:?}", e);
+        };
+
         let vdf_steps = oneshot_rx
             .await
             .expect("to receive VdfStepsReadGuard from GetVdfStateMessage message");
