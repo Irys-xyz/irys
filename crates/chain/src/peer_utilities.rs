cfc25433b0f012954656b3f5878f8f57b631b931
Craig Mayhew <craig@mayhew.io>
2025-05-21 11:13:33 +0100

    typo

--- crates/chain/src/peer_utilities.rs
+++ crates/chain/src/peer_utilities.rs
@@ -245,8 +245,8 @@ pub async fn sync_state_from_peers(
     let client = awc::Client::default();
     let peers = Arc::new(Mutex::new(trusted_peers.clone()));
 
-    // orignal comment "lets give the local api a few second to load..."
-    // I think this pause is actually to allow a few vdf steps to have occured...as it's removal causes the following types of errors:
+    // original comment "lets give the local api a few second to load..."
+    // I think this pause is actually to allow a few vdf steps to have occurred...as it's removal causes the following types of errors:
     //
     //     Block validation error Unavailable requested range (1..=2). Stored steps range is (1..=1)
     //     Block validation error No steps stored!
