e60572354ae293381783f061290330cb31f6385a
Craig Mayhew <craig@mayhew.io>
2025-05-22 13:18:20 +0100

    try for 10s

--- crates/actors/src/block_producer.rs
+++ crates/actors/src/block_producer.rs
@@ -524,7 +524,7 @@ impl Handler<SolutionFoundMessage> for BlockProducerActor {
             let parent = {
                 let mut attempts = 0;
                 loop {
-                    if attempts > 20 {
+                    if attempts > 50 {
                         break None;
                     }
 
