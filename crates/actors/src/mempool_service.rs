9c54231c3db9a4c802a3a044283c02936f082cc8
Craig Mayhew <craig@mayhew.io>
2025-05-26 10:57:15 +0100

    perf: removed superflous spawn

--- crates/actors/src/mempool_service.rs
+++ crates/actors/src/mempool_service.rs
@@ -888,11 +888,9 @@ impl Inner {
                 let gossip_sender = mempool_state_guard.gossip_tx.clone();
                 let gossip_data = GossipData::Chunk(chunk);
 
-                let _ = tokio::task::spawn(async move {
-                    if let Err(error) = gossip_sender.send(gossip_data).await {
-                        tracing::error!("Failed to send gossip data: {:?}", error);
-                    }
-                });
+                if let Err(error) = gossip_sender.send(gossip_data).await {
+                    tracing::error!("Failed to send gossip data: {:?}", error);
+                }
 
                 Ok(())
             }
