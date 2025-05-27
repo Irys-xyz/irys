64beddaec7a805cf656892b6ccb7035d4fa35008
Roberts Pumpurs <roberts@pumpurlabs.com>
2025-05-27 15:39:29 +0300

    feat: reth can fork

--- crates/irys-reth/src/lib.rs
+++ crates/irys-reth/src/lib.rs
@@ -1178,19 +1178,22 @@ mod tests {
     use alloy_eips::Encodable2718;
     use alloy_genesis::Genesis;
     use alloy_network::{EthereumWallet, TxSigner};
+    use alloy_primitives::Address;
     use alloy_primitives::{FixedBytes, Signature, TxKind, B256};
 
     use alloy_rpc_types::engine::PayloadAttributes;
     use alloy_signer_local::PrivateKeySigner;
     use eyre::OptionExt;
     use reth::{
-        api::{FullNodePrimitives, PayloadAttributesBuilder},
+        api::{FullNodePrimitives, NodeTypesWithDBAdapter, PayloadAttributesBuilder},
         args::{DiscoveryArgs, NetworkArgs, RpcServerArgs},
         builder::{rpc::RethRpcAddOns, FullNode, NodeBuilder, NodeConfig, NodeHandle},
+        consensus::FullConsensus,
         providers::{AccountReader, BlockHashReader, BlockNumReader},
         rpc::{api::eth::helpers::EthTransactions, server_types::eth::EthApiError},
         tasks::TaskManager,
     };
+    use reth_db::{test_utils::TempDatabase, DatabaseEnv};
     use reth_e2e_test_utils::{
         node::NodeTestContext, transaction::TransactionTestContext, wallet::Wallet, NodeHelperType,
     };
@@ -2565,4 +2568,198 @@ mod tests {
         );
         Ok(())
     }
+
+    #[test_log::test(tokio::test)]
+    async fn rollback_state_revert_on_fork_switch() -> eyre::Result<()> {
+        // Setup wallets and nodes (same block producer for both)
+        let wallets = Wallet::new(1).wallet_gen();
+        let wallets = EthereumWallet::from(wallets[0].clone()).default_signer();
+        let (mut nodes, _tasks, ..) = setup_irys_reth(
+            &[wallets.address(), wallets.address()],
+            custom_chain(),
+            false,
+            eth_payload_attributes,
+        )
+        .await?;
+        let mut node_b = nodes.pop().unwrap();
+        let mut node_a = nodes.pop().unwrap();
+
+        // Node A: advance 3 blocks, 2 system txs per block
+        let _block_hashes_a = advance_blocks(&mut node_a, 1, 3, 2, &wallets).await?;
+        let consistent_provider_a = node_a.inner.provider.consistent_provider().unwrap();
+        let account_a_on_fork = consistent_provider_a
+            .basic_account(&wallets.address())
+            .unwrap()
+            .unwrap();
+        // Node B: advance 4 blocks, 1 system tx per block
+        let block_hashes_b = advance_blocks(&mut node_b, 1, 4, 1, &wallets).await?;
+
+        dbg!(&block_hashes_b);
+        // Record Node B's state after 4 blocks
+        let consistent_provider_b = node_b.inner.provider.consistent_provider().unwrap();
+        let account_b = consistent_provider_b
+            .basic_account(&wallets.address())
+            .unwrap()
+            .unwrap();
+        let best_block_b = consistent_provider_b.best_block_number().unwrap();
+        let block_hash_b = consistent_provider_b
+            .block_hash(best_block_b)
+            .unwrap()
+            .unwrap();
+
+        // Node A switches forkchoice to Node B's latest block
+        node_a.sync_to(block_hash_b).await?;
+
+        // Node A's state should match Node B's
+        let consistent_provider_a = node_a.inner.provider.consistent_provider().unwrap();
+        let account_a = consistent_provider_a
+            .basic_account(&wallets.address())
+            .unwrap()
+            .unwrap();
+        let best_block_a = consistent_provider_a.best_block_number().unwrap();
+        let block_hash_a = consistent_provider_a.block_hash(4).unwrap().unwrap();
+        assert_eq!(best_block_b, 4);
+        assert_eq!(best_block_a, 4);
+        assert_eq!(block_hash_a, block_hash_b);
+        assert_ne!(
+            account_a_on_fork.balance, account_a.balance,
+            "balances for account a pre fork and post fork should be different!"
+        );
+        assert_ne!(
+            account_a_on_fork.nonce, account_a.nonce,
+            "balances for account a pre fork and post fork should be different!"
+        );
+        assert_eq!(
+            account_a.balance, account_b.balance,
+            "Balance should match after forkchoice switch"
+        );
+        assert_eq!(
+            account_a.nonce, account_b.nonce,
+            "Nonce should match after forkchoice switch"
+        );
+        assert_eq!(
+            best_block_a, best_block_b,
+            "Canonical block height should match after forkchoice switch"
+        );
+
+        Ok(())
+    }
+
+    async fn advance_blocks(
+        node: &mut IrysTestNode,
+        start_block: u64,
+        num_blocks: u64,
+        sys_txs_per_block: u64,
+        wallets: &Arc<dyn TxSigner<Signature> + Send + Sync>,
+    ) -> Result<Vec<FixedBytes<32>>, eyre::Error> {
+        let mut parent_blockhash = node
+            .inner
+            .provider
+            .consistent_provider()
+            .unwrap()
+            .block_hash(0)
+            .unwrap()
+            .unwrap();
+        let mut block_hashes = vec![parent_blockhash];
+        let mut nonce = 0;
+        for block_number in start_block..(start_block + num_blocks) {
+            for _tx_idx in 0..sys_txs_per_block {
+                let system_tx = block_reward(wallets.address(), block_number, parent_blockhash);
+                let system_tx_raw = compose_system_tx(nonce, 1, system_tx.clone());
+                nonce += 1;
+                let system_pooled_tx = sign_tx(system_tx_raw, wallets).await;
+                node.inner
+                    .pool
+                    .add_transaction(
+                        reth_transaction_pool::TransactionOrigin::Private,
+                        system_pooled_tx,
+                    )
+                    .await?;
+            }
+            let payload = node.advance_block().await?;
+            parent_blockhash = payload.block().hash();
+            block_hashes.push(parent_blockhash);
+        }
+        Ok::<_, eyre::Error>(block_hashes)
+    }
+
+    pub type IrysTestNode = NodeTestContext<
+        NodeAdapter<
+            reth::api::FullNodeTypesAdapter<
+                IrysEthereumNode,
+                Arc<TempDatabase<DatabaseEnv>>,
+                reth::providers::providers::BlockchainProvider<
+                    NodeTypesWithDBAdapter<IrysEthereumNode, Arc<TempDatabase<DatabaseEnv>>>,
+                >,
+            >,
+            reth::builder::components::Components<
+                reth::api::FullNodeTypesAdapter<
+                    IrysEthereumNode,
+                    Arc<TempDatabase<DatabaseEnv>>,
+                    reth::providers::providers::BlockchainProvider<
+                        NodeTypesWithDBAdapter<IrysEthereumNode, Arc<TempDatabase<DatabaseEnv>>>,
+                    >,
+                >,
+                reth_network::NetworkHandle,
+                Pool<
+                    TransactionValidationTaskExecutor<
+                        IrysEthTransactionValidator<
+                            reth::providers::providers::BlockchainProvider<
+                                NodeTypesWithDBAdapter<
+                                    IrysEthereumNode,
+                                    Arc<TempDatabase<DatabaseEnv>>,
+                                >,
+                            >,
+                            EthPooledTransaction,
+                        >,
+                    >,
+                    SystemTxsCoinbaseTipOrdering<EthPooledTransaction>,
+                    DiskFileBlobStore,
+                >,
+                evm::CustomEvmConfig,
+                Arc<dyn FullConsensus<EthPrimitives, Error = reth::consensus::ConsensusError>>,
+            >,
+        >,
+        EthereumAddOns<
+            NodeAdapter<
+                reth::api::FullNodeTypesAdapter<
+                    IrysEthereumNode,
+                    Arc<TempDatabase<DatabaseEnv>>,
+                    reth::providers::providers::BlockchainProvider<
+                        NodeTypesWithDBAdapter<IrysEthereumNode, Arc<TempDatabase<DatabaseEnv>>>,
+                    >,
+                >,
+                reth::builder::components::Components<
+                    reth::api::FullNodeTypesAdapter<
+                        IrysEthereumNode,
+                        Arc<TempDatabase<DatabaseEnv>>,
+                        reth::providers::providers::BlockchainProvider<
+                            NodeTypesWithDBAdapter<
+                                IrysEthereumNode,
+                                Arc<TempDatabase<DatabaseEnv>>,
+                            >,
+                        >,
+                    >,
+                    reth_network::NetworkHandle,
+                    Pool<
+                        TransactionValidationTaskExecutor<
+                            IrysEthTransactionValidator<
+                                reth::providers::providers::BlockchainProvider<
+                                    NodeTypesWithDBAdapter<
+                                        IrysEthereumNode,
+                                        Arc<TempDatabase<DatabaseEnv>>,
+                                    >,
+                                >,
+                                EthPooledTransaction,
+                            >,
+                        >,
+                        SystemTxsCoinbaseTipOrdering<EthPooledTransaction>,
+                        DiskFileBlobStore,
+                    >,
+                    evm::CustomEvmConfig,
+                    Arc<dyn FullConsensus<EthPrimitives, Error = reth::consensus::ConsensusError>>,
+                >,
+            >,
+        >,
+    >;
 }
