ae4c0269d12f5c93c66cd366f9397841f1784435
dmac <samuraidan@gmail.com>
2025-05-27 15:44:41 -0700

    fix: fixup merge

--- crates/chain/tests/utils.rs
+++ crates/chain/tests/utils.rs
@@ -36,13 +36,9 @@ use irys_types::{
     block_production::Seed, block_production::SolutionContext, Address, DataLedger, H256List, H256,
 };
 use irys_types::{
-    Base64, Base64, DatabaseProvider, DatabaseProvider, GossipData, IrysBlockHeader,
-    IrysBlockHeader, IrysTransaction, IrysTransaction, LedgerChunkOffset, LedgerChunkOffset,
-    PackedChunk, PackedChunk, PeerAddress, RethPeerInfo, UnpackedChunk, UnpackedChunk,
-};
-use irys_types::{
-    CommitmentTransaction, Config, IrysTransactionHeader, IrysTransactionId, NodeConfig, NodeMode,
-    TxChunkOffset,
+    Base64, CommitmentTransaction, Config, DatabaseProvider, GossipData, IrysBlockHeader,
+    IrysTransaction, IrysTransactionHeader, IrysTransactionId, LedgerChunkOffset, NodeConfig,
+    NodeMode, PackedChunk, PeerAddress, RethPeerInfo, TxChunkOffset, UnpackedChunk,
 };
 use irys_vdf::{step_number_to_salt_number, vdf_sha};
 use reth::rpc::types::engine::ExecutionPayloadEnvelopeV1Irys;
