67c5e2868b1bf273210c6b3705313921ea8e1551
Craig Mayhew <craig@mayhew.io>
2025-05-29 15:46:31 +0100

    fixed test by adding hack hack frmom PR #410

--- crates/actors/src/mempool_service.rs
+++ crates/actors/src/mempool_service.rs
@@ -589,10 +589,37 @@ impl Inner {
                 }
             }
 
+            // HACK HACK: in order for block discovery to validate incoming blocks
+            // it needs to read commitment tx from the database. Ideally it should
+            // be reading them from the mempool_service in memory cache, but we are
+            // putting off that work until the actix mempool_service is rewritten as a
+            // tokio service.
+            let mempool_state_read_guard = mempool_state.read().await;
+            match mempool_state_read_guard.irys_db.update_eyre(|db_tx| {
+                irys_database::insert_commitment_tx(db_tx, &commitment_tx)?;
+                Ok(())
+            }) {
+                Ok(()) => {
+                    info!(
+                        "Successfully stored commitment_tx in db {:?}",
+                        commitment_tx.id.0.to_base58()
+                    );
+                }
+                Err(db_error) => {
+                    error!(
+                        "Failed to store commitment_tx in db {:?}: {:?}",
+                        commitment_tx.id.0.to_base58(),
+                        db_error
+                    );
+                }
+            }
+
             // Gossip transaction
-            let mempool_state_guard = mempool_state.read().await;
-            let gossip_sender = mempool_state_guard.service_senders.gossip_broadcast.clone();
-            drop(mempool_state_guard);
+            let gossip_sender = mempool_state_read_guard
+                .service_senders
+                .gossip_broadcast
+                .clone();
+            drop(mempool_state_read_guard);
             let gossip_data = GossipData::CommitmentTransaction(commitment_tx.clone());
 
             gossip_sender
