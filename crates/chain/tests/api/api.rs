ea698816474de3e940fa585c7af531a546ab8b06
Craig Mayhew <craig@mayhew.io>
2025-05-29 14:30:54 +0100

    use correct and existing sender channels

--- crates/chain/tests/api/api.rs
+++ crates/chain/tests/api/api.rs
@@ -39,8 +39,6 @@ async fn heavy_api_end_to_end_test_256kb() {
 }
 
 async fn api_end_to_end_test(chunk_size: usize) {
-    let (ema_tx, _ema_rx) = tokio::sync::mpsc::unbounded_channel();
-    let (mempool_tx, _mempool_rx) = tokio::sync::mpsc::unbounded_channel();
     let entropy_packing_iterations = 1_000;
     let mut config = NodeConfig::testnet();
     config.consensus.get_mut().chunk_size = chunk_size.try_into().unwrap();
@@ -61,7 +59,7 @@ async fn api_end_to_end_test(chunk_size: usize) {
     node.node_ctx.start_mining().await.unwrap();
 
     let app_state = ApiState {
-        ema_service: ema_tx,
+        ema_service: node.node_ctx.service_senders.ema.clone(),
         reth_provider: node.node_ctx.reth_handle.clone(),
         reth_http_url: node
             .node_ctx
@@ -72,7 +70,7 @@ async fn api_end_to_end_test(chunk_size: usize) {
         block_index: node.node_ctx.block_index_guard.clone(),
         block_tree: node.node_ctx.block_tree_guard.clone(),
         db: node.node_ctx.db.clone(),
-        mempool_service: mempool_tx,
+        mempool_service: node.node_ctx.service_senders.mempool.clone(),
         peer_list: node.node_ctx.peer_list.clone(),
         chunk_provider: node.node_ctx.chunk_provider.clone(),
         config: config.into(),
