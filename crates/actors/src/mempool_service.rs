5a40237bef181023007c82347e2883d2bdb23896
Craig Mayhew <craig@mayhew.io>
2025-06-06 22:47:55 +0100

    load_persisted_state() now goes via MempoolServiceMessage::TxIngressMessage

--- crates/actors/src/mempool_service.rs
+++ crates/actors/src/mempool_service.rs
@@ -609,13 +609,15 @@ impl Inner {
     async fn load_persisted_state(&self) -> eyre::Result<()> {
         let read_tx = self.read_tx().await?;
         let txs = all_mempool_tx_headers(&read_tx)?;
-        read_tx.commit()?;
 
-        let mut guard = self.mempool_state.write().await;
         for tx in txs {
-            guard.valid_tx.insert(tx.id, tx);
+            let (oneshot_tx, oneshot_rx) = tokio::sync::oneshot::channel();
+            let tx_ingress_msg = MempoolServiceMessage::TxIngressMessage(tx.clone(), oneshot_tx);
+            if let Err(err) = self.service_senders.mempool.send(tx_ingress_msg) {
+                tracing::error!("error sending message to mempool: {:?}", err);
+            }
+            let _msg_result = oneshot_rx.await;
         }
-        drop(guard);
 
         self.irys_db.update_eyre(|tx| {
             clear_mempool_tx_headers(tx)?;
