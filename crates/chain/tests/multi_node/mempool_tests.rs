6d7acc9effd8eae0cc2273de78259179c86f2c4a
Craig Mayhew <craig@mayhew.io>
2025-06-12 16:12:07 +0100

    no need to send BlockConfirmedMessage

--- crates/chain/tests/multi_node/mempool_tests.rs
+++ crates/chain/tests/multi_node/mempool_tests.rs
@@ -422,11 +422,6 @@ async fn heavy_mempool_message_and_block_migration_test() -> eyre::Result<()> {
     assert_eq!(genesis_node.get_height().await, 0);
     genesis_node.mine_block().await.unwrap();
     let block = Arc::new(genesis_node.get_block_by_height(1).await?);
-    genesis_node
-        .node_ctx
-        .service_senders
-        .mempool
-        .send(MempoolServiceMessage::BlockConfirmedMessage(block.clone()))?;
 
     // ----- STAGE 3.1: show best txns were included in mined block
     let best = genesis_node.get_best_mempool_tx(None).await;
