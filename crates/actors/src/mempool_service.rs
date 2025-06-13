990270fb538c7e87141f4614c9138b4522e3f4b2
Craig Mayhew <craig@mayhew.io>
2025-06-13 17:11:37 +0100

    remove hack hack warning. we are working on it

--- crates/actors/src/mempool_service.rs
+++ crates/actors/src/mempool_service.rs
@@ -654,30 +654,6 @@ impl Inner {
                 }
             }
 
-            // // HACK HACK: in order for block discovery to validate incoming blocks
-            // // it needs to read commitment tx from the database. Ideally it should
-            // // be reading them from the mempool_service in memory cache, but we are
-            // // putting off that work until the actix mempool_service is rewritten as a
-            // // tokio service.
-            // match self.irys_db.update_eyre(|db_tx| {
-            //     irys_database::insert_commitment_tx(db_tx, &commitment_tx)?;
-            //     Ok(())
-            // }) {
-            //     Ok(()) => {
-            //         info!(
-            //             "Successfully stored commitment_tx in db {:?}",
-            //             commitment_tx.id.0.to_base58()
-            //         );
-            //     }
-            //     Err(db_error) => {
-            //         error!(
-            //             "Failed to store commitment_tx in db {:?}: {:?}",
-            //             commitment_tx.id.0.to_base58(),
-            //             db_error
-            //         );
-            //     }
-            // }
-
             // Gossip transaction
             self.service_senders
                 .gossip_broadcast
