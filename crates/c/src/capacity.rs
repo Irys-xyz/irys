901ba99506325a573867e12ed708d4c87bd2d630
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-06-15 18:11:34 +0000

    wip: fix lint exceptions for generated code

--- crates/c/src/capacity.rs
+++ crates/c/src/capacity.rs
@@ -1,6 +1,13 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(clippy::all)]
-#![allow(clippy::allow_attributes)]
+// and for some reason, clippy::all... doesn't actually cover all lints??
+#![allow(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::unused_trait_names,
+    clippy::needless_raw_strings,
+    clippy::unseparated_literal_suffix
+)]
 include!(concat!(env!("OUT_DIR"), "/capacity_bindings.rs"));
--- crates/c/src/lib.rs
+++ crates/c/src/lib.rs
@@ -1,12 +1,6 @@
-#![allow(clippy::allow_attributes)]
-// these are so we don't get clippy warnings on generated files
-#[allow(clippy::all)]
 pub mod capacity;
-#[allow(clippy::all)]
 pub mod capacity_single;
-#[allow(clippy::all)]
 pub mod vdf;
 
 #[cfg(feature = "nvidia")]
-#[allow(clippy::all)]
 pub mod capacity_cuda;
