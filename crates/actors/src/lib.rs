e46b65c305c29630c5b964e6c874cb2b23eaa1e0
Roberts Pumpurs <roberts@pumpurlabs.com>
2025-06-30 15:42:42 +0300

    fmt

--- crates/actors/src/lib.rs
+++ crates/actors/src/lib.rs
@@ -13,8 +13,8 @@ pub mod mining;
 pub mod packing;
 pub mod reth_service;
 pub mod services;
-pub mod storage_module_service;
 pub mod shadow_tx_generator;
+pub mod storage_module_service;
 pub mod validation_service;
 
 pub use addresses::*;
