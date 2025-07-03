2d55a2cdc7dd724f57ec5ca6cb5223c0f18d3087
Craig Mayhew <craig@mayhew.io>
2025-07-03 13:43:14 +0100

    unused use

--- crates/chain/tests/utils.rs
+++ crates/chain/tests/utils.rs
@@ -39,9 +39,9 @@ use irys_types::{
     partition::PartitionAssignment, Address, DataLedger, GossipBroadcastMessage, H256List, H256,
 };
 use irys_types::{
-    Base64, CommitmentTransaction, Config, DatabaseProvider, GossipRequest, IrysBlockHeader,
-    IrysTransaction, IrysTransactionHeader, IrysTransactionId, LedgerChunkOffset, NodeConfig,
-    NodeMode, PackedChunk, PeerAddress, RethPeerInfo, TxChunkOffset, UnpackedChunk,
+    Base64, CommitmentTransaction, Config, DatabaseProvider, IrysBlockHeader, IrysTransaction,
+    IrysTransactionHeader, IrysTransactionId, LedgerChunkOffset, NodeConfig, NodeMode, PackedChunk,
+    PeerAddress, RethPeerInfo, TxChunkOffset, UnpackedChunk,
 };
 use irys_vdf::state::VdfStateReadonly;
 use irys_vdf::{step_number_to_salt_number, vdf_sha};
