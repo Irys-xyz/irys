eebc0955a96aedc76c7e131743baae5fe7e74883
Craig Mayhew <craig@mayhew.io>
2025-07-15 13:39:30 +0100

    test for cases before and after block migration

--- crates/chain/tests/integration/test_double_spend.rs
+++ crates/chain/tests/integration/test_double_spend.rs
@@ -22,15 +22,16 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
         .start_and_wait_for_packing("GENESIS", seconds_to_wait)
         .await;
 
+    // TEST CASE 1: create a tx for later migration to block index,
+    //              testing it cannot be resumbitted back into mempool after migration
     // create and submit a data transaction
     let tx_data = vec![1u8; 64];
     let anchor = H256::zero();
-    let tx = node.post_data_tx(anchor, tx_data, &signer).await;
-    let txid = tx.header.id;
-    tracing::error!("txid: {:?}", txid);
+    let tx_for_migration = node.post_data_tx(anchor, tx_data, &signer).await;
+    let txid = tx_for_migration.header.id;
     node.wait_for_mempool(txid, seconds_to_wait).await?;
 
-    // mine block including tx
+    // mine block including tx_for_migration
     node.mine_block().await?;
     let block1 = node.get_block_by_height(1).await?;
     assert!(block1
@@ -39,7 +40,7 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
         .unwrap()
         .contains(&txid));
 
-    // mine enough blocks to cause block with txs to migrate to index
+    // mine enough blocks to cause block with tx_for_migration to migrate to index
     node.mine_blocks(
         config
             .consensus
@@ -49,14 +50,25 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
     )
     .await?;
 
-    // ensure block with tx is now in index
+    // TEST CASE 2: create a tx for the mempool,
+    //              testing it cannot be resubmitted to the mempool after mining
+    // create and submit a data transaction
+    let tx_data = vec![1u8; 64];
+    let anchor = txid; //chain from prior tx
+    let tx_for_mempool = node.post_data_tx(anchor, tx_data, &signer).await;
+    let txid = tx_for_mempool.header.id;
+    node.wait_for_mempool(txid, seconds_to_wait).await?;
+
+    // ensure block with tx_for_migration is now in index
     node.wait_until_block_index_height(block1.height, seconds_to_wait)
         .await?;
 
-    // resubmit same tx header
-    node.post_data_tx_raw(&tx.header).await;
+    // resubmit tx header that already exists in the database
+    node.post_data_tx_raw(&tx_for_migration.header).await;
+    // resubmit tx header that already exists in the mempool
+    node.post_data_tx_raw(&tx_for_mempool.header).await;
 
-    // ensure mempool does not accept duplicate
+    // ensure mempool does not accept either duplicate tx
     node.wait_for_mempool_shape(0, 0, 0, seconds_to_wait.try_into()?)
         .await?;
 
@@ -68,6 +80,6 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
         .get(&DataLedger::Submit)
         .unwrap()
         .contains(&txid));
-    panic!("show me some debug");
+
     Ok(())
 }
