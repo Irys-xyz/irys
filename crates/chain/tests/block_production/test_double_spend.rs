ab710de1bb03f8476338634041ce88e7d25c1628
Craig Mayhew <craig@mayhew.io>
2025-07-16 10:22:28 +0100

    WIP commitment txs currently crash a mining node in the case of a duplicate

--- crates/chain/tests/block_production/test_double_spend.rs
+++ crates/chain/tests/block_production/test_double_spend.rs
@@ -22,7 +22,10 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
         .start_and_wait_for_packing("GENESIS", seconds_to_wait)
         .await;
 
-    // TEST CASE 1: create a tx for later migration to block index,
+    // prepare anchors for commitment txs
+    let genesis_block = node.get_block_by_height(0).await?;
+
+    // TEST CASE 1: create a submit tx for later migration to block index,
     //              testing it cannot be resumbitted back into mempool after migration
     // create and submit a data transaction
     let tx_data = vec![1_u8; 64];
@@ -31,14 +34,29 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
     let txid = tx_for_migration.header.id;
     node.wait_for_mempool(txid, seconds_to_wait).await?;
 
+    // also create commitment txs that will later be resubmitted
+    tracing::error!("genesis block : {:?}", genesis_block);
+    let stake_for_migration = node.post_stake_commitment(genesis_block.block_hash).await;
+    let pledge_for_migration = node.post_pledge_commitment(stake_for_migration.id).await;
+    node.wait_for_mempool_commitment_txs(
+        vec![stake_for_migration.id, pledge_for_migration.id],
+        seconds_to_wait,
+    )
+    .await?;
+
+    tracing::error!("we make it here");
     // mine block including tx_for_migration
     node.mine_block().await?;
+    tracing::error!("node crashes mining a block and we never get here");
     let block1 = node.get_block_by_height(1).await?;
     assert!(block1
         .get_data_ledger_tx_ids()
         .get(&DataLedger::Submit)
         .unwrap()
         .contains(&txid));
+    let commitment_ids = block1.get_commitment_ledger_tx_ids();
+    assert!(commitment_ids.contains(&stake_for_migration.id));
+    assert!(commitment_ids.contains(&pledge_for_migration.id));
 
     // mine enough blocks to cause block with tx_for_migration to migrate to index
     node.mine_blocks(
@@ -47,6 +65,16 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
             .get_mut()
             .block_migration_depth
             .try_into()?,
+    )
+    .await?;
+
+    // create commitment txs that remain in the mempool
+    let block2_anchor = node.get_block_by_height(2).await?;
+    let stake_for_mempool = node.post_stake_commitment(block2_anchor.block_hash).await;
+    let pledge_for_mempool = node.post_pledge_commitment(stake_for_mempool.id).await;
+    node.wait_for_mempool_commitment_txs(
+        vec![stake_for_mempool.id, pledge_for_mempool.id],
+        seconds_to_wait,
     )
     .await?;
 
@@ -68,18 +96,30 @@ async fn heavy_double_spend_rejection_after_block_migration() -> eyre::Result<()
     // resubmit tx header that already exists in the mempool
     node.post_data_tx_raw(&tx_for_mempool.header).await;
 
-    // ensure mempool does not accept either duplicate tx
-    node.wait_for_mempool_shape(0, 0, 0, seconds_to_wait.try_into()?)
+    // resubmit commitment transactions that were already seen
+    node.post_commitment_tx(&stake_for_migration).await;
+    node.post_commitment_tx(&pledge_for_migration).await;
+    node.post_commitment_tx(&stake_for_mempool).await;
+    node.post_commitment_tx(&pledge_for_mempool).await;
+
+    // ensure mempool does not accept any duplicate tx
+    node.wait_for_mempool_shape(0, 0, 2, seconds_to_wait.try_into()?)
         .await?;
 
     // mine another block
     node.mine_block().await?;
-    let block2 = node.get_block_by_height(2).await?;
-    assert!(!block2
+    let block3 = node.get_block_by_height(3).await?;
+    assert!(!block3
         .get_data_ledger_tx_ids()
         .get(&DataLedger::Submit)
         .unwrap()
         .contains(&txid));
+
+    let commitment_ids = block3.get_commitment_ledger_tx_ids();
+    assert_eq!(
+        commitment_ids,
+        vec![stake_for_mempool.id, pledge_for_mempool.id]
+    );
 
     Ok(())
 }
