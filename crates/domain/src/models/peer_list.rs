0c22ec9bdf93151577a3d271160897631c0cea9a
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-08-23 13:36:12 +0000

    fix: update wait for peer log level

--- crates/domain/src/models/peer_list.rs
+++ crates/domain/src/models/peer_list.rs
@@ -169,9 +169,9 @@ impl PeerList {
                 return;
             }
 
-            // Check for active peers every second
-            debug!("waiting for active peers...");
-            tokio::time::sleep(Duration::from_secs(1)).await;
+            // Check for active peers
+            warn!("waiting for active peers...");
+            tokio::time::sleep(Duration::from_millis(500)).await;
         }
     }
 
