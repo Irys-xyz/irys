1ac3d2deb819f85d643fba5c33e4c4c11f3248cb
Jesse Cruz Wright <jesse.cruz.wright@gmail.com>
2025-08-23 16:00:28 +0100

    fix: sync changes (#690)
    * wip: add TODOs and code from call
    
    * fix: update wait for peer log level
    
    * chore: remove dead code

--- crates/p2p/src/block_pool.rs
+++ crates/p2p/src/block_pool.rs
@@ -444,11 +444,13 @@ where
                 "Block pool: Block {:?} is already being processed or fast-tracked, skipping",
                 block_header.block_hash
             );
-        } else {
-            self.blocks_cache
-                .add_block(Arc::clone(&block_header), skip_validation_for_fast_track)
-                .await;
+            return Err(BlockPoolError::AlreadyProcessed(block_header.block_hash));
         }
+
+        self.blocks_cache
+            .add_block(Arc::clone(&block_header), skip_validation_for_fast_track)
+            .await;
+
         debug!(
             "Block pool: Processing block {:?} (height {})",
             block_header.block_hash, block_header.height,
@@ -530,8 +532,10 @@ where
             current_block_hash
         );
 
+        // TODO: validate this UNTRUSTED height against the parent block's height (as we have processed it)
+
         self.block_status_provider
-            .wait_for_block_tree_to_catch_up(block_header.height)
+            .wait_for_block_tree_can_process_height(block_header.height)
             .await;
 
         if let Err(block_discovery_error) = self
