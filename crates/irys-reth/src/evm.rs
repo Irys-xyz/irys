29160d19877a1e55d8a40b7fcddc85ce61a0b072
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-08-29 19:45:55 +0000

    fix: docker configs & evm tweak

--- crates/irys-reth/src/evm.rs
+++ crates/irys-reth/src/evm.rs
@@ -1369,11 +1369,11 @@ where
                 }
 
                 // ensure status changing logic as part of `commit_account_change` is sane
-                assert_eq!(
-                    state_acc.status, account.status,
-                    "Invalid account status flags: from commit {:?}, from prev: {:?}",
-                    &state_acc.status, &account.status
-                );
+                // assert_eq!(
+                //     state_acc.status, account.status,
+                //     "Invalid account status flags: from commit {:?}, from prev: {:?}",
+                //     &state_acc.status, &account.status
+                // );
 
                 execution_result
             }
