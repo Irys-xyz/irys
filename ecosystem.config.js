63297014ca06d9eed14cceb0acad451c0edbc72d
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-08-31 22:15:45 +0000

    add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports =     // ecosystem.js
+{
+    "apps": [
+        {
+            "name": "Irys Node",
+            "script": "./target/debug-release/irys",
+            "exec": "none",
+            "exec_mode": "fork",
+            // "cron_restart": '*/45 * * * *',
+            // "cron_restart": 0,
+            "env": {
+                "kill_timeout": 5_000,
+                "RUST_LOG": "debug,jsonrpsee-server=off",
+                "RUST_BACKTRACE": "full"
+            }
+        }
+    ]
+};
