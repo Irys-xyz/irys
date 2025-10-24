f10166c2a614a7b44d3adbb1e10b0df1807543d0
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-10-24 20:11:53 +0000

    fix: fmt

--- crates/actors/src/mempool_service/ingress_proofs.rs
+++ crates/actors/src/mempool_service/ingress_proofs.rs
@@ -1,10 +1,6 @@
-use irys_database::{
-    tables::{CompactCachedIngressProof, IngressProofs},
-    walk_all,
-};
+use irys_database::tables::{CompactCachedIngressProof, IngressProofs};
 use irys_types::{ingress::CachedIngressProof, GossipBroadcastMessage, IngressProof};
 use reth_db::{transaction::DbTxMut as _, Database as _, DatabaseError};
-use tracing::debug;
 
 use crate::mempool_service::{IngressProofError, Inner};
 
