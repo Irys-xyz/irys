2fb6b40b64eae7f34dd8bacdaff6942eba53e9a7
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-11-12 14:25:40 +0000

    chore: add tracing TODO

--- crates/api-server/src/lib.rs
+++ crates/api-server/src/lib.rs
@@ -194,7 +194,7 @@ pub fn run_server(app_state: ApiState, listener: TcpListener) -> Server {
             .route("/", web::get().to(|| async { Redirect::to("/v1/info") }))
             .service(routes())
             .wrap(Cors::permissive())
-            .wrap(TracingLogger::default())
+            .wrap(TracingLogger::default()) // TODO: figure out how to change the `target` so we can easily filter HTTP and Gossip server logs
     })
     .listen(listener)
     .unwrap()
