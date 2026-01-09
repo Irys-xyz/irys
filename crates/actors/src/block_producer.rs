c9e3c0521a1043b5d5f716f480d6fa7bc8167685
jason <jason@ridgway-taylor.co.uk>
2026-01-09 07:24:49 +0000

    fix(hardfork): replace removed filtering

--- crates/actors/src/block_producer.rs
+++ crates/actors/src/block_producer.rs
@@ -1384,7 +1384,13 @@ pub trait BlockProdStrategy {
                 &self.inner().config.consensus,
             )?;
 
-        let commitment_txs = parent_commitment_snapshot.get_epoch_commitments();
+        // Filter epoch commitments by version if hardfork is active. Use the block's timestamp
+        let mut commitment_txs = parent_commitment_snapshot.get_epoch_commitments();
+        self.inner()
+            .config
+            .consensus
+            .hardforks
+            .retain_valid_commitment_versions(&mut commitment_txs, block_timestamp.to_secs());
 
         Ok(MempoolTxsBundle {
             // on epoch blocks we don't bill the end-user
