c558775c29233cb556a73e41f7684ec523795a1f
JesseTheRobot <jesse.cruz.wright@gmail.com>
2026-01-23 17:20:16 +0000

    chore: remove unused file

--- crates/database/src/db_index.rs
+++
@@ -1,5 +0,0 @@
-/// Once data has been well confirmed by being part of a transaction that is
-/// in a block with several confirmations, it can move out of the `db_cache`
-/// and into a `db_index` that is able to store more properties (to support
-/// mining) now that the data is confirmed.
-const _X: u64 = 1;
