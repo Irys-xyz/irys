729c2ca7613b5a9ce0c6d4f6f37a7f8c696448d8
Roberts Pumpurs <roberts@pumpurlabs.com>
2026-02-28 10:57:36 +0200

    inc worker threads

--- crates/chain/tests/utils.rs
+++ crates/chain/tests/utils.rs
@@ -390,7 +390,7 @@ impl IrysNodeTest<()> {
             };
 
         let runtime = tokio::runtime::Builder::new_multi_thread()
-            .worker_threads(1)
+            .worker_threads(2)
             .enable_all()
             .build()
             .expect("Failed to build dedicated tokio runtime");
