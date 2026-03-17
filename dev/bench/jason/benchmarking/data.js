aa95035f2253452a594e6e6ddc37cb32b2b27f4f
github-action-benchmark <github@users.noreply.github.com>
2026-03-17 15:21:44 +0000

    add Benchmark (cargo) benchmark result for f8088502cdc87043065ea7a780d78a1ee9a909fc

--- dev/bench/jason/benchmarking/data.js
+++ dev/bench/jason/benchmarking/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773758546573,
+  "lastUpdate": 1773760904982,
   "repoUrl": "https://github.com/Irys-xyz/irys",
   "entries": {
     "Benchmark": [
@@ -79,6 +79,86 @@ window.BENCHMARK_DATA = {
             "name": "apply_reset_seed",
             "value": 112,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Irys-xyz",
+            "username": "Irys-xyz"
+          },
+          "committer": {
+            "name": "Irys-xyz",
+            "username": "Irys-xyz"
+          },
+          "id": "f8088502cdc87043065ea7a780d78a1ee9a909fc",
+          "message": "feat(bench): add criterion benchmarks and CI workflow",
+          "timestamp": "2026-03-17T10:06:48Z",
+          "url": "https://github.com/Irys-xyz/irys/pull/1221/commits/f8088502cdc87043065ea7a780d78a1ee9a909fc"
+        },
+        "date": 1773760904123,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "vdf_sha/testing",
+            "value": 6073918,
+            "range": "± 23592",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "vdf_sha/testnet",
+            "value": 889885669,
+            "range": "± 17374409",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "vdf_sha/mainnet",
+            "value": 1131342769,
+            "range": "± 16114237",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "vdf_sha_verification/testing",
+            "value": 8460293,
+            "range": "± 177266",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "vdf_sha_verification/testnet",
+            "value": 1207881425,
+            "range": "± 7581216",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "vdf_sha_verification/mainnet",
+            "value": 1570421234,
+            "range": "± 20908338",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parallel_verification/testing",
+            "value": 3095919,
+            "range": "± 77424",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parallel_verification/testnet",
+            "value": 254970263,
+            "range": "± 2271629",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parallel_verification/mainnet",
+            "value": 333220050,
+            "range": "± 679830",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "apply_reset_seed",
+            "value": 113,
+            "range": "± 1",
             "unit": "ns/iter"
           }
         ]
