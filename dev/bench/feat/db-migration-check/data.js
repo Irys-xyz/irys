ed4683b099eac24b82b16a46e88ffaaec9c8033c
github-action-benchmark <github@users.noreply.github.com>
2026-03-18 23:44:42 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for cd2fd0aa23ef369bb0b3cfdae4e9be1297634c10

--- dev/bench/feat/db-migration-check/data.js
+++ dev/bench/feat/db-migration-check/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773830177126,
+  "lastUpdate": 1773877482192,
   "repoUrl": "https://github.com/Irys-xyz/irys",
   "entries": {
     "Benchmark": [
@@ -79,6 +79,86 @@ window.BENCHMARK_DATA = {
             "name": "apply_reset_seed",
             "value": 0.000113,
             "range": "± 0.0",
+            "unit": "ms/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Irys-xyz",
+            "username": "Irys-xyz"
+          },
+          "committer": {
+            "name": "Irys-xyz",
+            "username": "Irys-xyz"
+          },
+          "id": "cd2fd0aa23ef369bb0b3cfdae4e9be1297634c10",
+          "message": "feat: check database schema version on startup",
+          "timestamp": "2026-03-18T19:54:11Z",
+          "url": "https://github.com/Irys-xyz/irys/pull/1223/commits/cd2fd0aa23ef369bb0b3cfdae4e9be1297634c10"
+        },
+        "date": 1773877481192,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "vdf_sha/testing",
+            "value": 6.106535,
+            "range": "± 0.102493",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "vdf_sha/testnet",
+            "value": 878.546651,
+            "range": "± 17.638589",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "vdf_sha/mainnet",
+            "value": 1129.651197,
+            "range": "± 2.032533",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "vdf_sha_verification/testing",
+            "value": 8.430412,
+            "range": "± 0.036684",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "vdf_sha_verification/testnet",
+            "value": 1206.641201,
+            "range": "± 3.890085",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "vdf_sha_verification/mainnet",
+            "value": 1566.020182,
+            "range": "± 3.258778",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "parallel_verification/testing",
+            "value": 2.594328,
+            "range": "± 0.226602",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "parallel_verification/testnet",
+            "value": 259.793018,
+            "range": "± 3.309222",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "parallel_verification/mainnet",
+            "value": 332.356901,
+            "range": "± 3.696053",
+            "unit": "ms/iter"
+          },
+          {
+            "name": "apply_reset_seed",
+            "value": 0.000113,
+            "range": "± 0.000001",
             "unit": "ms/iter"
           }
         ]
